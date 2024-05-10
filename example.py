229fb1b7ca1f1b951fcb20d3ad8867055e2a34e5
Stefan Nožinić <stefan@lugons.org>
2024-05-10 18:49:04 +0200

    Add web interface for command execution

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
 from command_executor import command_executor_main, get_classes
-
-command_executor_main(get_classes("example_commands"))
+from command_executor.web import serve_web
+# command_executor_main(get_classes("example_commands"))
+serve_web(get_classes("example_commands"))

1769f8c1ecbf9c75058c97d1401ef9687ccbdb49
Stefan Nožinić <stefan@lugons.org>
2025-06-28 22:34:42 +0200

    Refactor REST API command handling to skip commands with 'no_http' tag and remove unused execute command endpoint

--- orgasm/http_rest.py
+++ orgasm/http_rest.py
@@ -1,7 +1,15 @@
 
+import http
 from orgasm import get_available_commands, get_command_specs, execute_command
+from orgasm import attr, tag 
 
 from flask import Flask, jsonify, request
+
+http_post = attr(http_method ="POST")
+http_get = attr(http_method ="GET")
+http_delete = attr(http_method ="DELETE")
+http_put = attr(http_method ="PUT")
+no_http = tag("no_http")
 
 def serve_rest_api(classes, port=5000):
     app = Flask(__name__)
@@ -10,6 +18,8 @@ def serve_rest_api(classes, port=5000):
     def command_specs():
         specs = get_command_specs(classes)
         for spec in specs:
+            if "no_http" in spec['tags']:
+                continue
             for arg in spec['args']:
                 if 'type' in arg:
                     if isinstance(arg['type'], tuple):
@@ -21,21 +31,16 @@ def serve_rest_api(classes, port=5000):
                         arg['help'] = arg['help'][1]
         return jsonify(specs)
 
-    @app.route('/execute/<command>', methods=['POST'])
-    def _execute_command(command):
-        if command not in get_available_commands(classes):
-            return jsonify({"error": "Command not found"}), 404
-        
-        A = request.json or {}
-        try:
-            result = execute_command(classes, command, **A)
-            return jsonify(result)
-        except Exception as e:
-            return jsonify({"error": str(e)}), 400
-
     for spec in get_command_specs(classes):
+        if "no_http" in spec['tags']:
+            print(f"Skipping command {spec['method_name']} due to 'no_http' tag")
+            continue
         # create endpoint for particular command
-        method = "GET" if len(spec["args"]) == 0 else "POST"        
+        method = "GET" if len(spec["args"]) == 0 else "POST"  
+        if "http_method" in spec["attrs"]:
+            method = spec["attrs"]["http_method"].upper()
+            if method not in ["GET", "POST", "PUT", "DELETE"]:
+                raise ValueError(f"Invalid HTTP method {method} for command {spec['method_name']}")      
         def command_endpoint(command=spec["method_name"]):
             print(f"Executing command: {command}")
             if request.method in ["GET", "DELETE"]:

9c83adbfb9c73f580b17049bc9b863f0cfabd6e6
Stefan Nožinić <stefan@lugons.org>
2025-07-05 03:11:58 +0200

    Bump version to 1.5.1 in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 setup(
     name='orgasm-package',
-    version='1.5',
+    version='1.5.1',
     packages=['orgasm'],
     author="Stefan Nožinić",
     author_email="stefan@lugons.org",

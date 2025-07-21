cb9006b69af96b20fdf3f9a43cd37328a74531e8
Stefan Nožinić <stefan@lugons.org>
2025-07-21 21:04:14 +0200

    Comment out serve_rest_api call in example.py

--- example.py
+++ example.py
@@ -4,7 +4,7 @@ from orgasm.web import serve_web
 from orgasm.http_rest import serve_rest_api
 from json import dumps
 # command_executor_main(get_classes("example_commands"), explicit_params=False)
-serve_rest_api(get_classes("example_commands"), port=5000)
+# serve_rest_api(get_classes("example_commands"), port=5000)
 # serve_web(get_classes("example_commands"))
 # command_executor_rpc(get_classes("example_commands"))
 
